c3975cb5415e9d3ddebf81d67c16f5751818583f
Yulia <derevyanko.julia@gmail.com>
2025-04-24 21:38:26 +0300

    redy

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,14 +1,15 @@
 import styles from "./Description.module.css";
 export default function Description() {
   return (
-    <div className="container">
-      <h1 className="title">
-        <span className="title_span">Sip</span> Happens Café
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title_span}>Sip</span> Happens Café
       </h1>
-      <p className="text">
-        Please leave your feedback <span className="text_span">about</span> our
-        service by selecting one <span className="text_span">of the</span>
-        options <span className="text_span">below</span>.
+      <p className={styles.text}>
+        Please leave your feedback <span className={styles.span}>about </span>
+        our service by selecting one{" "}
+        <span className={styles.span}> of the </span>
+        options <span className={styles.span}>below</span>.
       </p>
     </div>
   );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,38 @@
 import styles from "./Options.module.css";
-export default function Options() {
+export default function Options({ totalFB, onFeedbackAction }) {
   return (
     <>
-      <button className="button">Good</button>
-      <button className="button">Neutral</button>
-      <button className="button">Bad</button>
-      <button className="button">Reset</button>
+      <button
+        className={styles.button}
+        onClick={() => onFeedbackAction("good")}
+        datatype="good"
+      >
+        Good
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => onFeedbackAction("neutral")}
+        datatype="neutral"
+      >
+        Neutral
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => onFeedbackAction("bad")}
+        datatype="bad"
+      >
+        Bad
+      </button>
+
+      {totalFB > 0 && (
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => onFeedbackAction("reset")}
+        >
+          Reset
+        </button>
+      )}
     </>
   );
 }
